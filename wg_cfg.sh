f9bde4708648ca033ada637ae0091baa1f64b697
Tomas <108157159+tomasp-xyz@users.noreply.github.com>
2022-06-27 03:12:15 +0300

    Update wg_cfg.sh
    Added new code, will need to use function for y/n/cancel prompts

--- wg_cfg.sh
+++ wg_cfg.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+wg_dir="/etc/wireguard"
+
+#Define function to for yes/no/cancel prompts
+yn() {
+	
+
 #Check for sudo
 
 if [[ $UID -ne 0 ]]
@@ -18,3 +24,26 @@ while $(dpkg -s wireguard &> /dev/null) ; [[ $? -ne 0 ]]; do
 	esac
 done
 
+#Check if iptables is installed
+while $(dpkg -s iptables &> /dev/null) ; [[ $? -ne 0 ]]; do
+	read -p "iptables package is not installed. Do you wish to install it now? (y/n) " yn
+	case $yn in
+		[Yy]* ) apt update ; apt install iptables -y ;;
+		[Nn]* ) echo "Exiting script" && exit 1 ;;
+		* ) echo "Please answer yes or no (y/n)" >&2 ;;
+	esac
+done
+
+#Check if keys haven't been generated under the same filename already
+if [[ -f $wg_dir/privatekey && $wg_dir/publickey ]] ; then
+#need to use function here i think	
+fi
+#Generate WG keys
+$(cd $wg_dir && wg genkey | tee privatekey | wg pubkey > publickey) &> /dev/null
+echo "Your private key is (do not share this!):"
+cat $wg_dir/privatekey 
+echo
+echo "Your public key is (copy this to your peer/router!):"
+cat $wg_dir/publickey
+
+exit 0	
